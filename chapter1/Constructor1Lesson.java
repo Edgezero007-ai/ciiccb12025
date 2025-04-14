f2a4658430f274bcdb34fbb50d57447f78f7e4ee
marcoyimyaem <marcoyimyaem@gmail.com>
2025-04-14 03:44:24 +0000

    day 5 last closing

---
+++ chapter1/Constructor1Lesson.java
@@ -0,0 +1,23 @@
+package chapter1;
+
+public class Constructor1Lesson {
+    public static void main(String[] args) {
+        Chick c1 = new Chick();
+        Chick c2 = new Chick();
+        c2.name = "wakikik";
+        c1.Chick();
+        System.out.println("end of program");
+    }
+    
+}
+
+class Chick{
+    String name;
+    public Chick(){
+        System.out.println("creating new Chick Object");
+        this.name = "wakokok";
+    }
+    public void Chick(){
+        System.out.println("this is a method");
+    }
+}
---
+++ chapter1/Main.java
@@ -0,0 +1,22 @@
+package chapter1;
+
+import java.util.*;
+
+/** 
+ * plant object blahhhhh
+ * 
+ * 
+ */
+public class Main{
+    
+     static public void main(String[] name) {
+        Toy toy1 = new Toy();
+        toy1.setPrice(0);
+        Random r = new Random();
+        ArrayList al = new ArrayList();
+        Date date1 = new Date();
+    }
+        
+}
+
+
